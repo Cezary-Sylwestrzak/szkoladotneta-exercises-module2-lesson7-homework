ca625f60ce56288948dd855845c8222ed7105f88
Cezary <156573752+Cezary-Sylwestrzak@users.noreply.github.com>
2024-05-07 09:38:56 +0200

    "Zmiana wartości argumentu logicznego na dodatnią"

--- Program 3/Program.cs
+++ Program 3/Program.cs
@@ -30,7 +30,7 @@ namespace Program_3
             while (!userInput);
             
             // Instrukcja warunkowa sprawdzająca podaną liczbę.
-            if (someValue >= 0)
+            if (someValue >= 1)
             {
                 Console.WriteLine($"Liczba \"{someValue}\" jest liczbą dodatnią.");
             }
